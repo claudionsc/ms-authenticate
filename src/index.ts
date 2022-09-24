68a880851e54ac334df766a305a72d9601fb6aa5
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-24 10:48:16 -0300

    servidor http ocom typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import express, { Request, Response, NextFunction } from 'express'
+
+const app = express()
+
+
+app.get('/status', (req:Request , res:Response, next: NextFunction) => {
+    res.status(200).send({ foo: 'bar' })
+});
+
+app.listen(3000, () => {
+    console.log('Executando na porta 3000')
+})
