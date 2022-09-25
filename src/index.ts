989acf58b4ae60a7161339db1a0661218eeb3e45
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-25 14:02:53 -0300

    finalizando as rotas

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import usersRoute from './routes/users.route';
 const app = express()
 
 app.use(express.json())//middleware para interpretar json
+app.use(express.urlencoded({ extended: true}))
 
 app.use(usersRoute)
 
