20aeb25d59f3112a8f469c29aa16f58460262d40
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-25 15:55:46 -0300

    adicionando senha criptografada no sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+CREATE EXTENSION IF NOT EXISTS "pgcrypto"
 
 CREATE TABLE IF NOT EXISTS application_user(
     uuid uuid DEFAULT uuid_generate_v4(),
@@ -6,3 +7,5 @@ CREATE TABLE IF NOT EXISTS application_user(
     password VARCHAR NOT NULL,
     PRIMARY KEY (uuid)
 )
+
+INSERT INTO application_user (username, password) VALUES ('admin', crypt('admin', 'my_salt'))
