92bf802a0e77b77147e470e1c389143e3ae8fdd6
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-28 07:31:38 -0300

    query de consulta sem password

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,8 +1,15 @@
+import db from "../db"
 import User from "../models/user.model"
+
+db
 
 class UserRepository {
 
     findAllUsers(): User[] {
+        const sql = `
+            SELECT uuid, username 
+            FROM application_user 
+        `
         return [];
     }
     
